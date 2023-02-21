b894c656f1b5bbcbbc3a8d7876bdf86b0de1f63d
MrGrad <MrGrad@DESKTOP-T3AM2JH>
2023-02-21 22:19:40 +0300

    Add project files.

---
+++ Lecture3Exercise4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture3Exercise4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const double pi = 3.141;
+            double r = 5, a = 2;
+            double result = Math.Pow(r, a) * pi;
+            Console.WriteLine(result);
+            Console.ReadKey();
+
+        }
+    }
+}
